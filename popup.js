f1a47eabb7dc1d7447c3eb476cdc71aca3fc7971
Stefano J. Attardi <stefano@attardi.org>
2021-03-17 13:58:47 -0700

    Remove content script for dark mode detection

--- popup.js
+++ popup.js
@@ -1,3 +1,9 @@
+const match = matchMedia('(prefers-color-scheme: dark)');
+chrome.runtime.sendMessage({ scheme: match.matches ? 'dark' : 'light' });
+match.addListener(({ matches }) => {
+  chrome.runtime.sendMessage({ scheme: matches ? 'dark' : 'light' });
+});
+
 document.getElementById('copy-tab')?.addEventListener('click', (e) => {
   flashAndClosePopup(e.currentTarget);
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
--- toggleIcon.js
+++
@@ -1,5 +0,0 @@
-const match = matchMedia('(prefers-color-scheme: dark)');
-chrome.runtime.sendMessage({ scheme: match.matches ? 'dark' : 'light' });
-match.addListener(({ matches }) => {
-  chrome.runtime.sendMessage({ scheme: matches ? 'dark' : 'light' });
-});
